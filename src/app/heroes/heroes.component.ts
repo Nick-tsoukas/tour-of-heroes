0b83668030a2c0c6d54078af074c9267230a0076
Nick Tsoukas <nick.tsoukas101@gmail.com>
2019-02-13 10:33:52 -0600

    Adding click event to hero list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,12 +15,18 @@ export class HeroesComponent implements OnInit {
   };
 
   heroes = HEROES;
+  selectedHero: Hero;
+
 
 
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
